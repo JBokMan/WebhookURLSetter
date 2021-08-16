93efb678c47f5ca67d2edfba852e530193acb177
Julian Bokelmann <bokelmann@sipgate.de>
2021-08-16 17:14:21 +0200

    :bug: Fix path of script
    Co-authored-by: Fabian Siffert <siffert@sipgate.de>
    Co-authored-by: Manuel Arnold <arnold@sipgate.de>

--- set-io-webhook.sh
+++ set-io-webhook.sh
@@ -10,8 +10,11 @@ fi
 # start ngrok
 $(~/ngrok http 8080 --log=stdout > ngrok.log) &
 
+# set scriptpath to path of this shell script
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 # fetch ngrok URL
-URL=$(node NgrokUrlFetcher)
+URL=$(node $SCRIPTPATH/NgrokUrlFetcher)
 if [[ ! "$URL" == https://* ]] || [[ ! "$URL" == *.ngrok.io ]]
 then
 	echo 'NgrokUrlFetcher did not return a valid URL: ' $URL
@@ -23,10 +26,12 @@ fi
 # copy URL to clipboard
 echo -n $URL | xclip -selection clipboard
 
+# set scriptpath to path of this shell script
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 # set URL in sipgateio
-cd SipgateIoWebhookUrlSetter
-STATUS=$(node index.js $URL)
-if [ STATUS != 0 ]
+STATUS=$(node $SCRIPTPATH/SipgateIoWebhookUrlSetter $URL)
+if [ STATUS == 1 ]
 then
   echo 'Something went wrong setting the URL in sipgateio: ' $STATUS
   PID=$(pgrep -f ngrok)
